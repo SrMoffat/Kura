ee02d4bbdd01a8036a121282a115d590b3bbaa43
4fr0c0d3 <ngigemoffat@gmail.com>
2019-03-15 14:29:50 +0300

    chore(project-setup): add graphQL 👻
    - install graphql
    - install express-graphql
    - setup graphql schema, rootQuery, and rootMutation

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-// Instance of the app
+// instance of the app
 const app = express();
 
-// use the body-parser to parse the request body
+// body-parser to parse the request body
 app.use(bodyParser.json());
 
-// Basic endpoint 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-});
+// configure graphQL
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(
+        `
+        type RootQuery {
+            clusters: [String!]!            
+        }
+        type RootMutation {
+            createCluster(clusterName: String) : String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+        `
+    ),
+    // Resolvers/Controllers
+    rootValue: {
+        clusters: () => {
+            return [
+                'Sparks Society',
+                'Engineers Council',
+                'Invictus Society'
+            ]
+        },
+        createCluster: (args) => {
+            const clusterName = args.clusterName;
+            return clusterName;
+        }
+    },
+    graphiql: true
+}));
 
-// Listen to a specific port
+// listen to a specific port
 app.listen(3000);
